2f7199f1c1e0dd4184de5bb49bfbb6100567863b
0abdou0 <abdallaoui.abderrahmane.01@gmail.com>
2021-03-31 17:52:43 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+/*
+import "./App.css";
+import Jobs from "./component/Jobs";
+import Data from "./data.json";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.map((elem) => (
+        <Jobs data={elem} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import { useState } from "react";
+import "./App.css";
+import Jobs from "./component/Jobs";
+import Data from "./data.json";
+function App() {
+  const [counter, setConter] = useState(0);
+
+  const inc = () => {
+    if (counter == Data.length - 1) {
+      setConter(0);
+    } else {
+      setConter(counter + 1);
+    }
+  };
+
+  const dec = () => {
+    if (counter == 0) {
+      setConter(Data.length - 1);
+    } else {
+      setConter(counter - 1);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Jobs data={Data[counter]} />
+      <div>
+        <button style={{ color: "red", fontSize: 100 }} onClick={inc}>
+          +
+        </button>
+        <button style={{ color: "red", fontSize: 100 }} onClick={dec}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
